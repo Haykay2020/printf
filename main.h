a6197040f1293530fb114a1d71420c2154baf221
Adisa Abdulhakeem <yinkadisa@gmail.com>
2022-04-20 03:40:49 -0700

    prototypes file

--- main.h
+++ main.h
@@ -1,8 +1,37 @@
-#ifndef PRINTF
-#define PRINTF
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define INT_BITS (4 * 8)
+
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * struct call - struct call
+ * @t: t - flag for data type
+ * @f: function - assocated function
+ *
+ */
+typedef struct call
+{
+	char t;
+	int (*f)(char *, va_list, int);
+} call_t;
+
 int _printf(const char *format, ...);
-int _putchar(char c);
-int fun_string(va_list arguments);
-int fun_character(va_list arguments);
-int fun_integer(va_list arguments);
+int buff_append(char *buff_dest, va_list arg, int buff_count, char type);
+int print_buff(char *buff, unsigned int nbuff);
+int str_len(char *s);
+char *_strcpy(char *dest, char *src);
+int parse_char(char *buff_dest, va_list arg, int buff_count);
+int parse_string(char *buff_dest, va_list arg, int buff_count);
+int parse_int(char *buff_dest, va_list list, int buff_count);
+int parse_perc(char *buff_dest, va_list arg, int buff_count);
+int parse_binary(char *buff_dest, va_list arg, int buff_count);
+int parse_oct(char *buff_dest, va_list arg, int buff_count);
+int parse_hex(char *buff_dest, va_list arg, int buff_count);
+int parse_uint(char *buff_dest, va_list arg, int buff_count);
+
 #endif
